48ba5d3c0db653b2e1e6caf01282b6055ca189ff
leeyaowen <jcjh30101@gmail.com>
2020-10-23 17:35:29 +0800

    支援中文路徑 / 執行續優化
    *修改奇偶數辨識單位由資料夾改為tif檔案
    *增加輸出tif檔獨立input

--- TAI_replace_scale_accelerate.py
+++ TAI_replace_scale_accelerate.py
@@ -8,90 +8,99 @@ import threading
 
 
 def replace_scale_thread1():
-
-    for folders in folderlist[::2]:
-        replace_scale(folder=folders)
+    folder_index = 0
+    for folder in folderlist:
+        print('Start! - ' + folder + ' - thread1')
+        for filename in all_file[folder_index][::2]:
+            replace_scale(filenames=filename, folders=folder)
+        folder_index = folder_index + 1
 
 
 def replace_scale_thread2():
+    folder_index = 0
+    for folder in folderlist:
+        print('Start! - ' + folder + ' - thread2')
+        for filename in all_file[folder_index][1::2]:
+            replace_scale(filenames=filename, folders=folder)
+        folder_index = folder_index + 1
 
-    for folders in folderlist[1::2]:
-        replace_scale(folder=folders)
+
+def make_dir():
+    folder_index = 0
+    for folder in folderlist:
+        all_file.append(glob.glob('./' + InputPath + '/' + folder + '/*.tif'))
+
+        if len(all_file[folder_index]) == 0:
+            print(folder + 'no file!\n')
+        else:
+            os.makedirs(SavePath + '/' + folder, exist_ok=True)
+
+        folder_index = folder_index + 1
 
 
-def replace_scale(folder):
-    all_file = glob.glob('./' + InputPath + '/' + folder + '/*.tif')
+def replace_scale(filenames, folders):
+    try:
+        lock.acquire()
+        start = timeit.default_timer()
+        img_original = cv2.imdecode(np.fromfile(filenames, dtype='uint8'), 1)
+        lock.release()
+        img_original_gray = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
+        # img_original_gray_dn = cv2.fastNlMeansDenoising(img_original_gray, None, 20, 5, 15)
+        scale_original = cv2.imread('./scale/dpi300.tif', 0)
 
-    if len(all_file) == 0:
-        print(folder + 'no file!\n')
+        lock.acquire()
+        res = cv2.matchTemplate(img_original_gray, scale_original, cv2.TM_CCOEFF_NORMED)
+        min_val, max_val, min_point, max_point = cv2.minMaxLoc(res)
+        top_left = max_point
+        lock.release()
 
-    os.makedirs('./' + OutputPath + '/' + folder, exist_ok=True)
-
-    print('Start! - ' + folder)
-    for filename in all_file:
-
-        try:
+        if max_val < 0.5:
             lock.acquire()
-            start = timeit.default_timer()
-            img_original = cv2.imread(filename)
-            lock.release()
-            img_original_gray = cv2.cvtColor(img_original, cv2.COLOR_BGR2GRAY)
-            # img_original_gray_dn = cv2.fastNlMeansDenoising(img_original_gray, None, 20, 5, 15)
-            scale_original = cv2.imread('./scale/dpi300.tif', 0)
-
-            lock.acquire()
-            res = cv2.matchTemplate(img_original_gray, scale_original, cv2.TM_CCOEFF_NORMED)
-            min_val, max_val, min_point, max_point = cv2.minMaxLoc(res)
-            top_left = max_point
+            with open('val_lower_than_0point8.txt', mode='a') as F:
+                F.write(os.path.basename(filenames) + ' Max_val = ' + str(max_val) + ' / Path: ' + Family_folder + '/' + folders + '\n')
             lock.release()
 
-            if max_val < 0.5:
-                lock.acquire()
-                with open('val_lower_than_0point8.txt', mode='a') as F:
-                    F.write(os.path.basename(filename) + ' Max_val = ' + str(
-                        max_val) + ' / Path: ' + OutputPath + '/' + folder + '\n')
-                lock.release()
+        if max_val >= 0.5:
+            rgb_size = 30
+            rgb_values = img_original[top_left[1] - rgb_size:top_left[1], top_left[0]:top_left[0] + rgb_size]
 
-            if max_val >= 0.5:
-                rgb_size = 30
-                rgb_values = img_original[top_left[1] - rgb_size:top_left[1], top_left[0]:top_left[0] + rgb_size]
+            mean_blue = int(np.round(np.mean(rgb_values[:, :, 0])))
+            mean_green = int(np.round(np.mean(rgb_values[:, :, 1])))
+            mean_red = int(np.round(np.mean(rgb_values[:, :, 2])))
 
-                mean_blue = int(np.round(np.mean(rgb_values[:, :, 0])))
-                mean_green = int(np.round(np.mean(rgb_values[:, :, 1])))
-                mean_red = int(np.round(np.mean(rgb_values[:, :, 2])))
+            with Image.open(filenames) as base_image, Image.open(
+                    './scale/dpi300_alpha.tif') as scale_original_4c, Image.open('./scale/dpi600.tif') as watermark:
+                width, height = base_image.size
+                transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+                transparent.paste(base_image, (0, 0))
+                transparent.paste((mean_red, mean_green, mean_blue, 255), top_left, mask=scale_original_4c)
+                transparent.paste(watermark, top_left, mask=watermark)
+                transparent.save(SavePath + '/' + folders + '/' + os.path.basename(filenames), compression='tiff_jpeg', quality=100, dpi=(300, 300))
 
-                with Image.open(filename) as base_image, Image.open(
-                        './scale/dpi300_alpha.tif') as scale_original_4c, Image.open('./scale/dpi600.tif') as watermark:
-                    width, height = base_image.size
-                    transparent = Image.new('RGBA', (width, height), (0, 0, 0, 0))
-                    transparent.paste(base_image, (0, 0))
-                    transparent.paste((mean_red, mean_green, mean_blue, 255), top_left, mask=scale_original_4c)
-                    transparent.paste(watermark, top_left, mask=watermark)
-                    transparent.save('./' + OutputPath + '/' + folder + '/' + os.path.basename(filename),
-                                     compression='tiff_jpeg', quality=100, dpi=(300, 300))
+        del img_original
+        del img_original_gray
+        del scale_original
 
-            del img_original
-            del img_original_gray
-            del scale_original
+        stop = timeit.default_timer()
 
-            stop = timeit.default_timer()
+        print(os.path.basename(filenames) + ' is ok! / Max_val = ' + str(max_val) + ' / time : ' + str(stop - start))
 
-            print(os.path.basename(filename) + ' is ok! / Max_val = ' + str(max_val) + ' / time : ' + str(stop - start))
-
-        except Exception:
-            lock.release()
-            lock.acquire()
-            print('error in %s' % os.path.basename(filename))
-            with open('error.txt', mode='a') as F:
-                F.write('error in %s/%s/%s\n' % (InputPath, folder, os.path.basename(filename)))
-            lock.release()
-            continue
+    except Exception:
+        lock.release()
+        lock.acquire()
+        print('error in %s' % os.path.basename(filenames))
+        with open('error.txt', mode='a') as F:
+            F.write('error in %s/%s/%s\n' % (InputPath, folders, os.path.basename(filenames)))
+        lock.release()
 
 
 if __name__ == '__main__':
     InputPath = input('Input Path=?\n')
-    OutputPath = input('Output Path=?\n')
+    Family_folder = input('Family Folder=?\n')
+    SavePath = input('Img Save Path=?\n')
     folderlist = os.listdir(InputPath)
+    all_file = list()
+    make_dir()
     lock = threading.Lock()
     t1 = threading.Thread(target=replace_scale_thread1)
     t2 = threading.Thread(target=replace_scale_thread2)
